dbaafc7213a22acfbf6e4cf82bb19ef9af2f3556
Annaliisa Tongpattanakul <annaliisa@Annaliisas-MacBook-Pro.local>
2020-02-18 18:51:58 -0800

    can transfer user input/github profile info to create an html resume/profile page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const axios = require('axios');
 const inquirer = require('inquirer');
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer
     .prompt([{
@@ -11,12 +14,48 @@ inquirer
         message: 'What is your favorite color?',
         name: 'favoriteColor'
     },
-])
-    .then(({username, favoriteColor}) => {
+    ])
+    .then(({ username, favoriteColor }) => {
         const url = `https://api.github.com/users/${username}`;
         console.log(favoriteColor);
         axios.get(url)
             .then((res) => {
-               console.log(res.data);
+                // const answers = [res.data.name, res.data.bio, res.data.blog, res.data.public_repos]
+                console.log(res.data);
+                function generateHTML() {
+                    return `
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+                    <title>Document</title>
+                  </head>
+                  <body style="background-color: ${favoriteColor};">
+                    <div class="jumbotron jumbotron-fluid" style="background-color: ${favoriteColor};">
+                    <div class="container" size: 90% style="background-color: white;">
+                        <img src="${res.data.avatar_url}">
+                      <h1 class="display-4">Hi! My name is ${res.data.name}</h1>
+                      <p class="lead">I am from ${res.data.location}.</p>
+                      <h3>${res.data.bio}<span class="badge badge-secondary">Contact Me</span></h3>
+                      <ul class="list-group">
+                        <li class="list-group-item">My GitHub username is </li>
+                        <li class="list-group-item">LinkedIn: </li>
+                      </ul>
+                    </div>
+
+                  </div>
+                  </body>
+                  </html>`;
+                  }
+                    const html = generateHTML(res.data);
+                     return writeFileAsync("index.html", html);
+                  })
+                  .then(function() {
+                    console.log("Successfully wrote to index.html");
+                  })
+                  .catch(function(err) {
+                    console.log(err);
+                  });
             })
-    })
